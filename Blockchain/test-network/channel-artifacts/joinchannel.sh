67415f2c65b65d9a1efa4d105850b088d6b797b8
Pritam Singh <pkspritam10@gmail.com>
2020-04-03 20:40:24 +0530

    testing CI

--- Blockchain/test-network/channel-artifacts/joinchannel.sh
+++ Blockchain/test-network/channel-artifacts/joinchannel.sh
@@ -1,5 +1,5 @@
 
-peer channel create -f ./channel-artifacts/channel.tx -o orderer:7050 -c test
+peer channel create -f ./channel.tx -o orderer:7050 -c test
 peer channel join -b test.block
 
 echo $(peer channel list)
